7babc01e9af96aef8ef3c0034b77b33e5e9083ff
JasonJPu <cub1@live.ca>
2017-07-13 22:19:05 -0400

    Create React class

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // Create a new component. Should produce HTML
 const App = function () {
   return <div>Hi!</div>;
-}
+};
 
 // Take component's HTML and put in DOM
+ReactDOM.render(<App />);
