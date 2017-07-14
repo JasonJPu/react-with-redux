23f1f438b72cb609efcda0da5da57d824d15467b
JasonJPu <cub1@live.ca>
2017-07-13 22:23:24 -0400

    Finish component on DOM

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a new component. Should produce HTML
-const App = function () {
+const App = () => {
   return <div>Hi!</div>;
 };
 
 // Take component's HTML and put in DOM
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector(".container"));
