6fc4316d56cfa00f813377dfe89fbfe11e245481
JasonJPu <cub1@live.ca>
2017-07-15 11:01:42 -0400

    Set video search result to state

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.GOOGLE_API;
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
-
 // Create a new component. Should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // Take component's HTML and put in DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
