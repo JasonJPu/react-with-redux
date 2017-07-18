740292b437128d42ef4c96bef46be9900e123087
JasonJPu <cub1@live.ca>
2017-07-18 19:56:50 -0400

    Bind action creators

--- src/containers/book-list.jsx
+++ src/containers/book-list.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
   renderList() {
@@ -20,6 +22,12 @@ class BookList extends Component {
   }
 }
 
+BookList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
 function mapStateToProps(state) {
   // Whatever is returned will show up as props inside BookList
   return {
@@ -27,10 +35,15 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props on the BookList
+// container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed
+  // to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
 
-BookList.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-  })).isRequired,
-};
+// Promote BookList from a component to a container
+// It needs to know about this new dispatch method, selectBook. Make it available
+// as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
