c30952356d2b3c9707322e1c5d7f462cbef49c92
JasonJPu <cub1@live.ca>
2017-07-18 21:27:44 -0400

    Add conditional rendering

--- src/containers/book-detail.jsx
+++ src/containers/book-detail.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
     );
   }
 }
+
+BookDetail.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 function mapStateToProps(state) {
   return {
