bf8fbc12862f0cca74c068dfd91caea887a4bcf6
JasonJPu <cub1@live.ca>
2017-07-24 20:40:22 -0400

    Add redux starter for blog

---
+++ redux-blog/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        WEATHER_API: JSON.stringify(process.env.WEATHER_API),
+        GOOGLE_API: JSON.stringify(process.env.GOOGLE_API),
+      },
+    }),
+  ],
+};
