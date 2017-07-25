cd6b16113f4ce58ff001fa9a53c912e9a1ce63b1
JasonJPu <cub1@live.ca>
2017-07-24 22:51:27 -0400

    Basic routing

--- redux-blog/src/index.jsx
+++ redux-blog/src/index.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render() { return <div>Hello!</div>; }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div>; }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
